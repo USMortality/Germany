1f0708df82d88bd5a7364c9583669123b015e936
USMortality.com <usmortality@protonmail.com>
2022-05-03 14:18:52 -0400

    update to tsv

--- export.sh
+++ export.sh
@@ -16,16 +16,16 @@ mysql -h 127.0.0.1 -u root -e "SET GLOBAL sql_mode = '';"
 
 # Deutschland
 mysql -h 127.0.0.1 -u root deutschland <query/d/query.sql
-mysql -h 127.0.0.1 -u root deutschland <query/d/mortality_quarter.sql >./out/d/mortality_quarter.csv
-mysql -h 127.0.0.1 -u root deutschland <query/d/mortality_yearly_current_week.sql >./out/d/mortality_yearly_current_week.csv
-mysql -h 127.0.0.1 -u root deutschland <query/d/mortality_yearly.sql >./out/d/mortality_yearly.csv
-mysql -h 127.0.0.1 -u root deutschland <query/d/time_series.sql >./out/d/time_series.csv
-mysql -h 127.0.0.1 -u root deutschland <query/d/time_series_age.sql >./out/d/time_series_age.csv
+mysql -h 127.0.0.1 -u root deutschland <query/d/mortality_quarter.sql >./out/d/mortality_quarter.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/d/mortality_yearly_current_week.sql >./out/d/mortality_yearly_current_week.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/d/mortality_yearly.sql >./out/d/mortality_yearly.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/d/time_series.sql >./out/d/time_series.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/d/time_series_age.sql >./out/d/time_series_age.tsv
 
 # Bundesländer
 mysql -h 127.0.0.1 -u root deutschland <query/bl/query.sql
-mysql -h 127.0.0.1 -u root deutschland <query/bl/mortality_quarter.sql >./out/bl/mortality_quarter.csv
-mysql -h 127.0.0.1 -u root deutschland <query/bl/mortality_yearly.sql >./out/bl/mortality_yearly.csv
-mysql -h 127.0.0.1 -u root deutschland <query/bl/mortality_yearly_current_week.sql >./out/bl/mortality_yearly_current_week.csv
-mysql -h 127.0.0.1 -u root deutschland <query/bl/quartal.sql >./out/bl/quartal.csv
-mysql -h 127.0.0.1 -u root deutschland <query/bl/flu_season_2020_2022.sql >./out/bl/flu_season_2020_2022.csv
+mysql -h 127.0.0.1 -u root deutschland <query/bl/mortality_quarter.sql >./out/bl/mortality_quarter.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/bl/mortality_yearly.sql >./out/bl/mortality_yearly.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/bl/mortality_yearly_current_week.sql >./out/bl/mortality_yearly_current_week.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/bl/quartal.sql >./out/bl/quartal.tsv
+mysql -h 127.0.0.1 -u root deutschland <query/bl/flu_season_2020_2022.sql >./out/bl/flu_season_2020_2022.tsv
